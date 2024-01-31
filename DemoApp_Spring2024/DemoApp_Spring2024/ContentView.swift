0b9dd3fb6e583ae83b7ca3aa018a132017143ad7
fpellicano <fpellicano@newhaven.edu>
2024-01-30 20:49:45 -0500

    initial commit of all necessary project files

---
+++ DemoApp_Spring2024/DemoApp_Spring2024/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  DemoApp_Spring2024
+//
+//  Created by Frank Pellicano on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, class!")
+        }
+        .padding()
+    }
+    //
+    //
+    //
+    //
+    //
+    
+}
+///
+#Preview {
+    ContentView()
+}
