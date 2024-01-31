0939d0c9920d65a04d1cc5325e78a82367fe7868
fpellicano <fpellicano@newhaven.edu>
2024-01-30 20:42:35 -0500

    Initial Comment on main file

---
+++ DemoApp_Spring2024/DemoApp_Spring2024/DemoApp_Spring2024App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp_Spring2024App.swift
+//  DemoApp_Spring2024
+//
+//  Created by Frank Pellicano on 1/30/24.
+//
+
+import SwiftUI
+// Main entry point for our app
+@main
+struct DemoApp_Spring2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
